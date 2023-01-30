33b65cb5fafb80e6978192ec6762305931bf1aea
Leija Salmivirta <leija.salmivirta@gmail.com>
2023-01-30 16:19:56 +0200

    Added functions to app.tsx to start the games

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import { Button, Container, Stack } from "@mui/material";
+import Heading from "./components/Heading";
+import CompetitionsList from "./components/CompetitionsList";
+import AddContestant from "./components/AddContestants";
+import GameDialog from "./components/GameDialog";
+import EndDialog from "./components/EndDialog";
+import DetailedDialog from "./components/DetailedDialog";
+import { CompetitionsContext } from "./context/CompetitionsContext";
+import AddDartsDialog from "./components/AddDartsDialog";
+import DartsGameDialog from "./components/DartsGameDialog";
+import EndDartsDialog from "./components/EndDartsDialog";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const { setAddDialog, setAddDartsDialog, setGameName } =
+      useContext(CompetitionsContext);
+
+   const addMokkitikka = (): void => {
+      setAddDialog(true);
+      setGameName("Mökkitikka");
+   };
+
+   const addDarts = (): void => {
+      setAddDartsDialog(true);
+      setGameName("Darts 501");
+   };
+
+   return (
+      <Container>
+         <Stack spacing={2}>
+            <Heading />
+
+            <Button variant="contained" onClick={() => addMokkitikka()}>
+               Start Mökkitikka
+            </Button>
+            <Button variant="contained" onClick={() => addDarts()}>
+               Start Darts 501
+            </Button>
+            <CompetitionsList />
+            <AddContestant />
+            <AddDartsDialog />
+            <GameDialog />
+            <EndDialog />
+            <DartsGameDialog />
+            <EndDartsDialog />
+            <DetailedDialog />
+         </Stack>
+      </Container>
+   );
 }
 
 export default App;
